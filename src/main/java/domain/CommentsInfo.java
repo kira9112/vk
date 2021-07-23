5c94d6e2c04c6cf1593a3d0a22a1874c14fd82ff
Kira9112 <kira91_911@mail.ru>
2021-07-23 07:35:07 +0300

    modified java.files

--- src/main/java/domain/CommentsInfo.java
+++ src/main/java/domain/CommentsInfo.java
@@ -3,4 +3,7 @@ package domain;
 public class CommentsInfo {
     private int count;
     private int canPost;
+    private int groupsCanPost;
+    private boolean canClose;
+    private boolean canOpen;
 }
--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -3,6 +3,18 @@ package domain;
 public class Post {
     private int id;
     private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private String text;
+    private ViewsInfo viewsInfo;
     private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
 
 }
