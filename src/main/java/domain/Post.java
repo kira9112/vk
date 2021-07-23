ad67f70cb083ff19ea6b5e555d84c1ff92bfd19a
Kira9112 <kira91_911@mail.ru>
2021-07-23 08:37:24 +0300

    update java.files

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -1,8 +1,25 @@
 package domain;
 
+import domain.CommentsInfo;
+import domain.LikesInfo;
+import domain.RepostsInfo;
+
 public class Post {
     private int id;
     private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private String text;
+    private ViewsInfo viewsInfo;
     private CommentsInfo commentsInfo;
+    private LikesInfo likesInfo;
+    private RepostsInfo repostsInfo;
+
 
 }
---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,15 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] search;
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+    }
+
+}
